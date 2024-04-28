cee5dd606252cd7029f003f881f6c4e28e4be8f7
Lucas Coser <lucas.coser@gmail.com>
2024-04-28 19:40:32 +0200

    Inclusion of files for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# The function below creates a mechanism to cache a matrix and its inverse. You
+# can set the matrix using the set function, retrieve the matrix using the get
+# function, set the inverse using the setinverse function, and retrieve the
+# inverse using the getinverse function.
 
+# Defines the function named makeCacheMatrix. It takes an optional argument x,
+# which defaults to an empty matrix if not provided.
+makeCacheMatrix <- function(x = matrix()) {
+        
+        # Sets a variable i to store the matrix inverse with NULL initially
+        i <- NULL
+        
+        # The set function is defined within makeCacheMatrix. It takes one
+        # argument y, representing a matrix. Inside the function, it assigns
+        # the input matrix y to the variable x using <<-, which is the
+        # assignment operator that assigns a value to a variable in an
+        # environment that is different from the current environment (i.e.,
+        # it assigns y to x in the parent environment of the function).
+        # It also resets the inverse (i) to NULL.
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        # get returns the cached matrix x, i.e., it simply returns the current
+        # value of x.
+        get <- function() x
+        
+        # setinverse takes one argument inverse, which is the inverse of the
+        # matrix. It assigns the inverse to the variable i using <<-, similar
+        # to how set assigns y to x.
+        setinverse <- function(inverse) i <<- inverse
+        
+        # getinverse returns the cached inverse i, i.e., it simply returns the
+        # current value of i
+        getinverse <- function() i
+        
+        # Creates a list containing the four functions defined above: set, get,
+        # setinverse, and getinverse. It returns this list.
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
---
+++ cachematrix2.R
@@ -0,0 +1,95 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+# Defines the 'makeCacheMatrix' function without any arguments
+makeCacheMatrix <- function() {
+        
+        # Initializes a variable 'cache' to store the matrix.
+        cache <- NULL
+
+        # The function 'set' is defined within 'makeCacheMatrix'. It takes one
+        # argument 'matrix', representing a matrix. Inside the function, it
+        # assigns the input matrix 'matrix' to the variable 'cache' using '<<-',
+        # and it also resets the inverse to NULL
+        set <- function(matrix) {
+                cache <<- matrix
+                inverse <<- NULL
+        }
+        
+        # The function 'get' returns the current value of 'cache'
+        get <- function() {
+                cache
+        }
+
+        # The function 'setinverse' takes one argument 'inverse_matrix', which
+        # represents the inverse of the matrix. Inside the function, it assigns
+        # the input inverse matrix to the variable 'inverse' using the operator
+        # '<<-'
+        setinverse <- function(inverse_matrix) {
+                inverse <<- inverse_matrix
+        }
+        
+        # The function 'getinverse' returns the current value of 'inverse'
+
+        getinverse <- function() {
+                inverse
+        }
+        
+        #. Creates a list containing the four functions defined above, i.e.,
+        # 'set', 'get', 'setinverse', and 'getinverse'
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+## Write a short comment describing this function
+
+# Defines the 'cacheSolve' function, which takes at least one argument 'x',
+# representing the cache matrix object. The `...` notation allows additional
+# arguments to be passed to the 'solve' function
+
+cacheSolve <- function(x, ...) {
+
+        # Retrieves the cached inverse matrix ('i') from the cache matrix object
+        # 'x' using the 'getinverse' function defined in the 'makeCacheMatrix'
+        # function
+        i <- x$getinverse()
+
+        # This conditional statement checks if the inverse matrix 'i' is not
+        # NULL. If it's not NULL, it means that the inverse matrix is already
+        # cached, and there's no need to recompute it. In that case, it prints
+        # a message indicating that cached data is being retrieved and returns
+        # the cached inverse matrix.
+        if (!is.null(i)) {
+
+                message("Getting cached data")
+                
+                return(i)
+        }
+
+        # If the inverse matrix is NULL, this line retrieves the original matrix
+        # ('data') from the cache matrix object 'x' using the 'get' function
+        data <- x$get()
+
+        # Computes the inverse matrix 'i' using the 'solve' function applied to
+        # the original matrix 'data'. The '...' notation allows additional
+        # arguments to be passed to the 'solve' function
+        i <- solve(data, ...)
+
+        # After computing the inverse matrix 'i', caches the inverse matrix by
+        # setting it in the cache matrix object 'x' using the 'setinverse'
+        # function
+        x$setinverse(i)
+
+        # Finally, the computed inverse matrix 'i' is returned as the result
+        # of the function
+        i
+}
+
+
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix()
+B1$set(B)
+
+inverse_B <- B1$getinverse()
+B1$setinverse(inverse_B)
